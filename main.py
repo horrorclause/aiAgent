81cfc0f6da66568de429f00eb898a9755bf81077
d3lvx <m4j3r@poton.me>
2025-06-24 13:13:17 -0400

    added cli check for prompt

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os
+import os, sys
 from dotenv import load_dotenv
 from google import genai
 
@@ -6,14 +6,17 @@ load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
+if len(sys.argv) == 1:
+    print('No message was entered, next time drop one in quotes like this: "Hey there".\nExiting now, bye.')
+    sys.exit(1)
+else:
+    cl_contents = sys.argv[1]
 
 response = client.models.generate_content(
     model='gemini-2.0-flash-001',
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
-    
+    contents= cl_contents
 )
 
-
-print(response.text)
-print(f"Prompt tokans: {response.usage_metadata.prompt_token_count}")
+print('\n',response.text)
+print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
 print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
