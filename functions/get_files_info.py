da15bb22af877685a215e6107a3e9742fcd767bb
d3lvx <m4j3r@poton.me>
2025-07-08 16:26:15 -0400

    update

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -19,8 +19,20 @@ def get_files_info(working_directory, directory=None):
         
         if os.path.isdir(directory) is False:
             return f'Error: "{directory}" is not a directory'
+        
+        directory_contents = os.listdir(directory) # Stores contents in list
+        
+        if len(directory_contents) != 0:
+            for entry in directory_contents:
+                file_name = entry
+                file_size = directory + file_name
+                is_dir = os.path.isdir(directory + file_name)
+
+                print(f'- {file_name}: file_size={file_size} bytes, is_dir={is_dir}')
+
             
 
     except Exception as e:
-        return f'Something happened: "{e}"'
+        return f'Error: "{e}"'
     
+
