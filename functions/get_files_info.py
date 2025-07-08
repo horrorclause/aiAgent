fd428f25eba037b4560fff925d70fc0ff4bd127e
d3lvx <m4j3r@poton.me>
2025-07-08 15:40:11 -0400

    Added security check

---
+++ functions/get_files_info.py
@@ -0,0 +1,21 @@
+import os
+
+def get_files_info(working_directory, directory=None):
+    
+    try:
+        if directory is not None:
+            directory = os.path.join(working_directory, directory)
+        
+        else:
+            directory = working_directory
+
+        ap_working_dir = os.path.abspath(working_directory) # Absolute path of working directory
+        ap_directory = os.path.abspath(directory) # Absolute Path of directory
+
+        if not ap_directory.startswith(ap_working_dir):
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+            
+
+    except Exception as e:
+        return f'Something happened: {e}'
+    
