fb5d1f0ae89ef5f58a89be97ec5e7f7eb985991a
d3lvx <m4j3r@poton.me>
2025-07-08 18:00:23 -0400

    added comments to tests.py

---
+++ functions/get_file_content.py
@@ -0,0 +1,28 @@
+import os
+
+def get_file_content(working_directory, file_path):
+    supplied_file_path = file_path
+
+    try:
+        if file_path is not None:
+            file_path = os.path.join(working_directory,file_path)
+        
+        abs_working_dir = os.path.abspath(working_directory)
+        abs_file_path = os.path.abspath(file_path)
+
+        if not abs_file_path.startswith(abs_working_dir): # Check if absolute file path is prepended with working dir
+            return f'Error: Cannot read "{supplied_file_path}" as it is outside the permitted working directory'
+        
+        is_file = os.path.isfile(file_path)
+        if is_file == False:
+            #print(os.path.join(working_directory,file_path))
+            #print(full_file_path)
+            return f'Error: File not found or is not a regular file: "{supplied_file_path}"'
+
+        with open(file_path, "r") as file:
+            read_file = file.readlines()
+            return "".join(read_file)
+        
+        read_file.close()
+    except Exception as e:
+        return f"Error: {e}"
--- tests.py
+++ tests.py
@@ -1,25 +1,34 @@
+# Test script for /functions/get_files_info.py and get_file_contents.py
+
 from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
-# Test scenario #1
-fileInfo = get_files_info("calculator", ".")
-print(f"Result for current directory:\n{fileInfo}")
+# # Test scenario #1
+# fileInfo = get_files_info("calculator", ".")
+# print(f"Result for current directory:\n{fileInfo}")
 
-# Test scenario #2
-fileInfo2 = get_files_info("calculator", "pkg")
-print(f"Result for pkg directory:\n{fileInfo2}")
+# # Test scenario #2
+# fileInfo2 = get_files_info("calculator", "pkg")
+# print(f"Result for pkg directory:\n{fileInfo2}")
 
-# Test scenario #3
-fileInfo3 = get_files_info("calculator", "/bin" )
-print(f"Result for the /bin directory:\n{fileInfo3}")
+# # Test scenario #3
+# fileInfo3 = get_files_info("calculator", "/bin" )
+# print(f"Result for the /bin directory:\n{fileInfo3}")
 
-# Test scenario #4
-fileInfo4 = get_files_info("calculator", "../" )
-print(f"Result for ../ :\n{fileInfo4}")
+# # Test scenario #4
+# fileInfo4 = get_files_info("calculator", "../" )
+# print(f"Result for ../ :\n{fileInfo4}")
 
-# Test scenario #5
-fileInfo5 = get_files_info(".", "calculator" )
-print(f"Result for calculator directory:\n{fileInfo5}")
+# # Test scenario #5
+# fileInfo5 = get_files_info(".", "calculator" )
+# print(f"Result for calculator directory:\n{fileInfo5}")
 
-# Test scenario #6
-fileInfo6 = get_files_info(".", "." )
-print(f"Result for aiAgent directory:\n{fileInfo6}")
+# # Test scenario #6
+# fileInfo6 = get_files_info(".", "." )
+# print(f"Result for aiAgent directory:\n{fileInfo6}")
+
+print('\n[+] Beginning "get_file_content" tests:\n')
+
+# Test Scenario #1
+file = get_file_content("calculator", "lorem.txt")
+print(f'Result for "lorem.txt" file:\n{file}\n')
