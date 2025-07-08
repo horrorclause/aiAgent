397b58590e28a8b898275fb9f52e8d063505b3a3
d3lvx <m4j3r@poton.me>
2025-07-08 18:43:37 -0400

    updated test.py

--- tests.py
+++ tests.py
@@ -3,31 +3,35 @@
 from functions.get_files_info import get_files_info
 from functions.get_file_content import get_file_content
 
-# # Test scenario #1
-# fileInfo = get_files_info("calculator", ".")
-# print(f"Result for current directory:\n{fileInfo}")
+print('\n\n[+] Beginning "get_files_info" tests:\n')
 
-# # Test scenario #2
-# fileInfo2 = get_files_info("calculator", "pkg")
-# print(f"Result for pkg directory:\n{fileInfo2}")
+# Test scenario #1
+fileInfo = get_files_info("calculator", ".")
+print(f"Result for current directory:\n{fileInfo}")
 
-# # Test scenario #3
-# fileInfo3 = get_files_info("calculator", "/bin" )
-# print(f"Result for the /bin directory:\n{fileInfo3}")
+# Test scenario #2
+fileInfo2 = get_files_info("calculator", "pkg")
+print(f"Result for pkg directory:\n{fileInfo2}")
 
-# # Test scenario #4
-# fileInfo4 = get_files_info("calculator", "../" )
-# print(f"Result for ../ :\n{fileInfo4}")
+# Test scenario #3
+fileInfo3 = get_files_info("calculator", "/bin" )
+print(f"Result for the /bin directory:\n{fileInfo3}")
 
-# # Test scenario #5
-# fileInfo5 = get_files_info(".", "calculator" )
-# print(f"Result for calculator directory:\n{fileInfo5}")
+# Test scenario #4
+fileInfo4 = get_files_info("calculator", "../" )
+print(f"Result for ../ :\n{fileInfo4}")
 
-# # Test scenario #6
-# fileInfo6 = get_files_info(".", "." )
-# print(f"Result for aiAgent directory:\n{fileInfo6}")
+# Test scenario #5
+fileInfo5 = get_files_info(".", "calculator" )
+print(f"Result for calculator directory:\n{fileInfo5}")
 
-print('\n[+] Beginning "get_file_content" tests:\n')
+# Test scenario #6
+fileInfo6 = get_files_info(".", "." )
+print(f"Result for aiAgent directory:\n{fileInfo6}")
+
+print('\n\n[-] Ending "get_files_info" tests [-]\n')
+
+print('\n\n[+] Beginning "get_file_content" tests:\n')
 
 # Test Scenario #1
 print('\n[+] Test Scenario #1\n')
@@ -49,3 +53,4 @@ print('\n[+] Test Scenario #4\n')
 file4 = get_file_content("calculator", "/bin/cat")
 print(f'Result for "/bin/cat" file:\n{file4}\n')
 
+print('\n\n[-] Ending "get_file_content" tests [-]\n')
